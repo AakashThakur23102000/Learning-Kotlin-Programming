03f9c572d5225dce7599fa075a1dea485e8e9f50
Aakash Thakur <aakash.thakur@sharajman.com>
2025-03-19 18:36:14 +0530

    Added March 19 2025 learning.

--- Kotlin/CaptainGame/app/src/main/java/com/example/captaingame/MainActivity.kt
+++ Kotlin/CaptainGame/app/src/main/java/com/example/captaingame/MainActivity.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.captaingame.ui.theme.CaptainGameTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CaptainGameTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    CaptainGame(innerPadding)
                 }
             }
         }
@@ -31,17 +38,66 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CaptainGame(innerPadding: PaddingValues = PaddingValues(0.dp)) {
+    var treasureFound by remember { mutableIntStateOf(0) }
+    var direction by remember { mutableStateOf("North") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)
+    ) {
+        Text(text = "Treasure Found - $treasureFound")
+        Text(text = "Direction - $direction")
+
+        Button(
+            onClick = {
+                direction = "East"
+                if (Random.nextBoolean()) {
+                    treasureFound += 1
+                }
+            }
+        ) {
+            Text(text = "Sale East")
+        }
+
+        Button(
+            onClick = {
+                direction = "West"
+                if (Random.nextBoolean()) {
+                    treasureFound += 1
+                }
+            }
+        ) {
+            Text(text = "Sale West")
+        }
+
+        Button(
+            onClick = {
+                direction = "North"
+                if (Random.nextBoolean()) {
+                    treasureFound += 1
+                }
+            }
+        ) {
+            Text(text = "Sale North")
+        }
+
+        Button(
+            onClick = {
+                direction = "South"
+                if (Random.nextBoolean()) {
+                    treasureFound += 1
+                }
+            }
+        ) {
+            Text(text = "Sale South")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CaptainGameTheme {
-        Greeting("Android")
-    }
+fun CaptainGamePreview() {
+    CaptainGame()
 }
--- Kotlin/UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ Kotlin/UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.unitconverter
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -14,7 +13,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.paddingFrom
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
@@ -27,9 +25,14 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
@@ -55,6 +58,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverterComp(modifier: Modifier = Modifier) {
+
+    var inputValue by remember { mutableStateOf("") }
+    var outputValue by remember { mutableStateOf("") }
+    var inputUnit by remember { mutableStateOf("Centimeters") }
+    var outputUnit by remember { mutableStateOf("Centimeters") }
+    var iExpanded by remember { mutableStateOf(false) }
+    var oExpanded by remember { mutableStateOf(false) }
+    var conversionFactor = remember { mutableStateOf(0.01) }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -65,80 +77,131 @@ fun UnitConverterComp(modifier: Modifier = Modifier) {
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        OutlinedTextField(value = "", onValueChange = {
-            // Here goes onChangeValue
-        })
-
+        OutlinedTextField(
+            value = inputValue,
+            onValueChange = { text ->
+                inputValue = text
+            },
+            label = { Text(text = "Enter value") }
+        )
         Spacer(modifier = Modifier.height(16.dp))
 
         Row {
-            // Here elements are stacked next to each other
-
-//            //code to show toast when button is pressed
-//            val context = LocalContext.current
-//            Button(
-//                onClick = {
-//                    Toast.makeText(
-//                        context, "Toast is showing up.", Toast.LENGTH_LONG
-//                    ).show()
-//                }
-//            ) {
-//                Text(text = "Press Me")
-//            }
-
 
             Box() {
-                Button(onClick = {/*Fill later*/ }) {
+                Button(onClick = { iExpanded = !iExpanded }) {
                     Text(text = "Select")
                     Icon(
                         Icons.Default.ArrowDropDown,
                         contentDescription = "Drop Down Arrow Down",
-
-                        )
+                    )
                 }
                 DropdownMenu(
-                    expanded = true,
+                    modifier = Modifier
+                        .background(color = Color.Yellow),
+
+                    expanded = iExpanded,
                     onDismissRequest = {/* */ }
                 ) {
                     DropdownMenuItem(
                         text = {
                             Text(text = "Centimeters")
                         },
-                        onClick = {/**/ }
+                        onClick = {
+                            iExpanded = !iExpanded
+                            inputUnit = "Centimeters"
+                        }
                     )
                     DropdownMenuItem(
                         text = {
                             Text(text = "Meters")
                         },
-                        onClick = {/**/ }
+                        onClick = {
+                            iExpanded = !iExpanded
+                            inputUnit = "Meters"
+                        }
                     )
                     DropdownMenuItem(
                         text = {
                             Text(text = "Feet")
                         },
-                        onClick = {/**/ }
+                        onClick = {
+                            iExpanded = !iExpanded
+                            inputUnit = "Feet"
+                        }
                     )
                     DropdownMenuItem(
                         text = {
                             Text(text = "Millimeters")
                         },
-                        onClick = {/**/ }
+                        onClick = {
+                            iExpanded = !iExpanded
+                            inputUnit = "Millimeters"
+                        }
                     )
                 }
             }
             Spacer(modifier = Modifier.width(16.dp))
             Box() {
-                Button(onClick = {/*Fill later*/ }) {
+                Button(onClick = { oExpanded = !oExpanded }) {
                     Text(text = "Select")
                     Icon(
                         Icons.Default.ArrowDropDown,
                         contentDescription = "Drop Down Arrow Down",
                     )
                 }
+
+                DropdownMenu(
+                    expanded = oExpanded,
+                    onDismissRequest = {/* */ }
+                ) {
+                    DropdownMenuItem(
+                        text = {
+                            Text(text = "Centimeters")
+                        },
+                        onClick = {
+                            oExpanded = !oExpanded
+                            outputUnit = "Centimeters"
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = {
+                            Text(text = "Meters")
+                        },
+                        onClick = {
+                            oExpanded = !oExpanded
+                            outputUnit = "Meters"
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = {
+                            Text(text = "Feet")
+                        },
+                        onClick = {
+                            oExpanded = !oExpanded
+                            outputUnit = "Feet"
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = {
+                            Text(text = "Millimeters")
+                        },
+                        onClick = {
+                            oExpanded = !oExpanded
+                            outputUnit = "Millimeters"
+                        }
+                    )
+                }
+
             }
 
         }
-
+        Text(
+            text = "Input Unit: -  $inputUnit"
+        )
+        Text(
+            text = "Output Unit: - $outputUnit"
+        )
         Text(
             modifier = Modifier.padding(18.dp),
             text = "Result: "
